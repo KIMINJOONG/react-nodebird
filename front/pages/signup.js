e7c5d8f44a708bfa9e669ce6646f2fad051276b6
KIMINJOONG <ijboym216@gmail.com>
2019-05-05 16:40:00 +0900

    use _app.js and segregate layout

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,78 +1,58 @@
-import React, { useState } from "react";
-import AppLayout from "../components/AppLayout";
-import Head from "next/head";
+import React, { useState, useCallback } from "react";
 import { Form, Input, Checkbox, Button } from "antd";
 
 const Signup = () => {
-  const [id, setId] = useState("");
-  const [nick, setNick] = useState("");
-  const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [termError, setTermError] = useState(false);
-
-  const onSubmit = e => {
-    e.preventDefault();
-    if (password !== passwordCheck) {
-      return setPasswordError(true);
-    }
-    if (!term) {
-      return setTermError(true);
-    }
-    console.log({
-      id,
-      nick,
-      password,
-      passwordCheck,
-      term
-    });
-  };
-
-  const onChangeId = e => {
-    setId(e.target.value);
-  };
-
-  const onChangeNick = e => {
-    setNick(e.target.value);
-  };
-
-  const onChangePassword = e => {
-    setPassword(e.target.value);
-  };
-
-  const onChangePasswordChk = e => {
-    setPasswordError(e.target.value !== password);
-    setPasswordCheck(e.target.value);
-  };
-
-  const onChangeTerm = e => {
-    setTermError(false);
-    setTerm(e.target.checked);
-  };
-
   // 커스텀 훅 사용법
   const useInput = (initValue = null) => {
     const [value, setter] = useState(initValue);
-    const handler = e => {
+    const handler = useCallback(e => {
       setter(e.target.value);
-    };
+    }, []);
     return [value, handler];
   };
 
+  const [id, onChangeId] = useInput("");
+  const [nick, onChangeNick] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (password !== passwordCheck) {
+        return setPasswordError(true);
+      }
+      if (!term) {
+        return setTermError(true);
+      }
+      console.log({
+        id,
+        nick,
+        password,
+        passwordCheck,
+        term
+      });
+    },
+    [password, passwordCheck, term]
+  );
+  const onChangePasswordChk = useCallback(
+    e => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
+
+  const onChangeTerm = useCallback(e => {
+    setTermError(false);
+    setTerm(e.target.checked);
+  }, []);
+
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
-        />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js" />
-      </Head>
-      <AppLayout>
-        <div>회원가입</div>
-      </AppLayout>
       <Form onSubmit={onSubmit} style={{ padding: 10 }}>
         <div>
           <label htmlFor="user-id">아이디</label>
