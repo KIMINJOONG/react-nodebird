94691a6fa177e0d3a32b8bdd3d13df8f39325269
KIMINJOONG <ijboym216@gmail.com>
2019-05-07 19:15:46 +0900

    make main screen

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,78 @@
 import React from "react";
+import { Form, Input, Button, Card, Icon, Avatar } from "antd";
+
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [
+    {
+      User: {
+        id: 1,
+        nickname: "제주주"
+      },
+      content: "첫번째 게시물",
+      img:
+        "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726"
+    }
+  ]
+};
 
 const Home = () => {
   return (
-    <>
-      <div>Hello, Next!</div>
-    </>
+    <div>
+      {dummy.isLoggedIn && (
+        <Form style={{ marginBottom: 20 }} encType="multipart/form-data">
+          <Input.TextArea
+            maxLength={140}
+            placeholder="어떤 신기한 일이 있었나요?"
+          />
+          <div>
+            <input type="file" mutiple hidden />
+            <Button>이미지 업로드</Button>
+            <Button type="primary" style={{ float: "right" }} htmlType="submit">
+              짹짹
+            </Button>
+          </div>
+          <div>
+            {dummy.imagePaths.map((v, i) => {
+              return (
+                <div key={v} style={{ display: "inline-block" }}>
+                  <img
+                    src={`http://localhost:3000/${v}`}
+                    style={{ width: "200px" }}
+                    alt={v}
+                  />
+                  <div>
+                    <Button>제거</Button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Form>
+      )}
+      {dummy.mainPosts.map(c => {
+        return (
+          <Card
+            key={+c.createdAt}
+            cover={c.img && <img alt="example" src={c.img} />}
+            actions={[
+              <Icon type="retweet" key="retweet" />,
+              <Icon type="heart" key="heart" />,
+              <Icon type="message" key="message" />,
+              <Icon type="ellipsis" key="ellipsis" />
+            ]}
+            extra={<Button>팔로우</Button>}
+          >
+            <Card.Meta
+              avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
+              title={c.User.nickname}
+              description={c.content}
+            />
+          </Card>
+        );
+      })}
+    </div>
   );
 };
 
