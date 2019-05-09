a026278f467cb21ce79db3b4d9034253dcbb58f8
KIMINJOONG <ijboym216@gmail.com>
2019-05-09 17:26:34 +0900

    Components segregate

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu, Input, Row, Col, Card, Avatar } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 import LoginForm from "../components/LoginForm";
+import UserProfile from "./UserProfile";
 const dummy = {
   nickname: "김인중",
   Post: [],
@@ -30,40 +31,15 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {dummy.isLoggedIn ? (
-            <Card
-              actions={[
-                <div key="twit">
-                  짹짹
-                  <br />
-                  {dummy.Post.length}
-                </div>,
-                <div key="following">
-                  팔로잉
-                  <br />
-                  {dummy.Followings.length}
-                </div>,
-                <div key="follow">
-                  팔로워
-                  <br />
-                  {dummy.Followers.length}
-                </div>
-              ]}
-            >
-              <Card.Meta
-                avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
-                title={dummy.nickname}
-              />
-            </Card>
-          ) : (
-            <LoginForm />
-          )}
+          {dummy.isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
         </Col>
         <Col xs={24} md={6}>
-          세번째
+          <Link href="https://github.com/KIMINJOONG">
+            <a target="_blank">깃허브</a>
+          </Link>
         </Col>
       </Row>
     </div>
