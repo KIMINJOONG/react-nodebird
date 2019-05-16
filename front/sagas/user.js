22b5a709fd702262bb6def651bd9970c4a35efb5
KIMINJOONG <ijboym216@gmail.com>
2019-05-16 11:16:39 +0900

    understand about Saga's generator

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,5 +1,7 @@
-import { all, fork, takeLatest } from "redux-saga/effects";
+import { all, fork, takeLatest, call, put, take } from "redux-saga/effects";
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../reducers/user";
+
+const HELLO_SAGA = "HELLO_SAGA";
 
 function loginAPI() {
   //서버에 요청을 보내는부분
@@ -25,6 +27,16 @@ function* watchLogin() {
   yield takeLatest(LOG_IN, login);
 }
 
+function* helloSaga() {
+  console.log("before saga");
+  // while -> eventListner를 동기적으로 표현했다고 이해하기
+  while (true) {
+    yield take(HELLO_SAGA);
+    console.log("hello saga");
+    // 비동기 요청, 타이머 넣어도 가능
+  }
+}
+
 export default function* userSaga() {
-  yield all([fork(watchLogin)]);
+  yield helloSaga();
 }
