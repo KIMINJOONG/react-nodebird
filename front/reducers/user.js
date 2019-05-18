e28a6d974e4c04e0847ba67203df233a5135580c
KIMINJOONG <ijboym216@gmail.com>
2019-05-18 13:54:50 +0900

    saga patern and Q&A

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -12,11 +12,18 @@ export const initialState = {
   user: null
 };
 
-export const SIGN_UP = "SGIN_UP";
-export const LOG_IN = "LOG_IN"; // 액션의 이름
-export const LOG_OUT = "LOG_OUT";
+// 비동기 요청
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
+
+// 동기요청
 
 // 동적인 데이터는 함수로 만들어줌 signup.js도 참고할것
 export const signUpAction = data => {
@@ -35,21 +42,29 @@ export const logoutAction = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOG_IN: {
+    case LOG_IN_REQUEST: {
+      return {
+        ...state,
+        loginData: action.data,
+        isLoading: true
+      };
+    }
+    case LOG_IN_SUCCESS: {
       return {
         ...state,
         isLoggedIn: true,
-        user: dummyUser
+        user: dummyUser,
+        isLoading: false
       };
     }
-    case LOG_OUT: {
+    case LOG_OUT_REQUEST: {
       return {
         ...state,
         isLoggedIn: false,
         user: null
       };
     }
-    case SIGN_UP: {
+    case SIGN_UP_REQUEST: {
       return {
         ...state,
         signUpData: action.data
