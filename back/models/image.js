cdfd7ad360b20fe3327063648ebfeee91e85bcad
KIMINJOONG <ijboym216@gmail.com>
2019-05-27 10:38:57 +0900

    Sequelize and ERD

---
+++ back/models/image.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      nickname: {
+        type: DataTypes.STRING(20),
+        allowNull: false // 필수
+      },
+      userId: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true // 고유한값
+      },
+      password: {
+        type: DataTypes.STRING(100), // 100글자 이하
+        allowNull: false
+      }
+    },
+    {
+      charset: "utf8",
+      collate: "utf8-_general_ci" //두개를 해줘야 한글이 됨
+    }
+  );
+  User.associate = db => {
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment); // 한명이 (게시글, 코멘트)여러개를 쓸 수 있다.
+  };
+  return User;
+};
