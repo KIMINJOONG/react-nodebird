0eafa7c8d3aa1e0fd080d97235cf72f9c970ed0a
KIMINJOONG <ijboym216@gmail.com>
2019-05-28 09:08:37 +0900

    connect sequelize and db but i have permission problem

--- back/models/user.js
+++ back/models/user.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    "User",
+    "User", // 테이블명은 Users로 만들어짐
     {
       nickname: {
         type: DataTypes.STRING(20),
@@ -22,8 +22,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
   User.associate = db => {
-    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Post, { as: "Posts" });
     db.User.hasMany(db.Comment); // 한명이 (게시글, 코멘트)여러개를 쓸 수 있다.
+    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" }); // through=중간테이블명, belongsToMany는 as를 넣어주는게 좋다
+    db.User.belongsToMany(db.User, { through: "Follow", as: "Followers" }); // as 는 이름이 같을시 구별용도로
+    db.User.belongsToMany(db.User, { through: "Follow", as: "Followings" });
   };
   return User;
 };
