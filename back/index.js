f65e5b6ff82af347dbfd3b1f3ea1e4e498be66b4
KIMINJOONG <ijboym216@gmail.com>
2019-05-29 20:45:46 +0900

    lets make a backend server api

--- back/index.js
+++ back/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const userAPIRouter = require("./routes/user");
+const postAPIRouter = require("./routes/post");
+const postsAPIRouter = require("./routes/posts");
+const 
 const db = require("./models");
 const app = express();
 db.sequelize.sync();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world");
-});
+// API는 다른 서비스가 내 서비스의 기능을 실행할수 있게 열어둔 창구
+app.use("/api/user", userAPIRouter);
+app.use("/api/post", postAPIRouter);
+app.use("/api/posts", postsAPIRouter);
 
-app.get("/about", (req, res) => {
-  res.send("Hello, about");
-});
 
 app.listen(3065, () => {
   console.log(`server is running on http://localhost:3065`);
---
+++ back/routes/post.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/api/post", (req, res) => {});
+
+router.post("/api/post/images", (req, res) => {});
+
+module.exports = router;
