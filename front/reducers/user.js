475b4091ff94ebcf84de3ea348a9595e37cb0cfa
KIMINJOONG <ijboym216@gmail.com>
2019-07-05 22:29:59 +0900

    nickname update

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -10,7 +10,9 @@ export const initialState = {
   me: null, // 내 정보
   followingList: [], //팔로잉 리스트
   followerList: [], // 팔로워 리스트
-  userInfo: null //남의 정보
+  userInfo: null, //남의 정보
+  isEditingNickname: false,
+  editNicknameErrorReason: ''
 };
 
 // 비동기 요청
@@ -30,9 +32,13 @@ export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
 export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
 export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-export const LOAD_FOLLOW_REQUEST = "LOAD_FOLLOW_REQUEST";
-export const LOAD_FOLLOW_SUCCESS = "LOAD_FOLLOW_SUCCESS";
-export const LOAD_FOLLOW_FAILURE = "LOAD_FOLLOW_FAILURE";
+export const LOAD_FOLLOWERS_REQUEST = "LOAD_FOLLOWERS_REQUEST";
+export const LOAD_FOLLOWERS_SUCCESS = "LOAD_FOLLOWERS_SUCCESS";
+export const LOAD_FOLLOWERS_FAILURE = "LOAD_FOLLOWERS_FAILURE";
+
+export const LOAD_FOLLOWINGS_REQUEST = "LOAD_FOLLOWINGS_REQUEST";
+export const LOAD_FOLLOWINGS_SUCCESS = "LOAD_FOLLOWINGS_SUCCESS";
+export const LOAD_FOLLOWINGS_FAILURE = "LOAD_FOLLOWINGS_FAILURE";
 
 export const FOLLOW_USER_REQUEST = "FOLLOW_USER_REQUEST";
 export const FOLLOW_USER_SUCCESS = "FOLLOW_USER_SUCCESS";
@@ -47,6 +53,11 @@ export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
 export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
 
 export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
+
+export const EDIT_NICKNAME_REQUEST = 'EDIT_NICKNAME_REQUEST';
+export const EDIT_NICKNAME_SUCCESS = 'EDIT_NICKNAME_SUCCESS';
+export const EDIT_NICKNAME_FAILURE = 'EDIT_NICKNAME_FAILURE';
+
 
 // 동기요청
 
@@ -144,6 +155,45 @@ const reducer = (state = initialState, action) => {
         ...state,
       }
     }
+    case FOLLOW_USER_REQUEST: {
+      return {
+        ...state,
+      }
+    }
+    case FOLLOW_USER_SUCCESS: {
+      return{
+        ...state,
+        me: {
+          ...state.me,
+          Followings: [{id: action.data}, ...state.me.Followings],
+        },
+      };
+    }
+    case FOLLOW_USER_FAILURE: {
+      return {
+        ...state
+      }
+    }
+    case UNFOLLOW_USER_REQUEST: {
+      return{
+        ...state
+      }
+    }
+    case UNFOLLOW_USER_SUCCESS: {
+      return {
+        ...state,
+        me: {
+          ...state.me,
+          Followings: state.me.Followings.filter(v => v.id !== action.data),
+        },
+        followingList: state.followingList.filter(v => v.id !== action.data),
+      };
+    }
+    case UNFOLLOW_USER_FAILURE: {
+      return {
+        ...state
+      }
+    }
     case ADD_POST_TO_ME: {
       return {
         ...state,
@@ -152,6 +202,82 @@ const reducer = (state = initialState, action) => {
           Posts: [{ id: action.data }, ...state.me.Posts],
         }
       };
+    }
+    case LOAD_FOLLOWERS_REQUEST: {
+      return {
+        ...state
+      }
+    }
+    case LOAD_FOLLOWERS_SUCCESS: {
+      return {
+        ...state,
+        followerList: action.data
+      }
+    }
+    case LOAD_FOLLOWERS_FAILURE: {
+      return {
+        ...state
+      }
+    }
+    case LOAD_FOLLOWINGS_REQUEST: {
+      return {
+        ...state
+      }
+    }
+    case LOAD_FOLLOWINGS_SUCCESS: {
+      return {
+        ...state,
+        followingList: action.data
+      }
+    }
+    case LOAD_FOLLOWINGS_FAILURE: {
+      return {
+        ...state
+      }
+    }
+    case REMOVE_FOLLOWER_REQUEST: {
+      return {
+        ...state
+      }
+    }
+    case REMOVE_FOLLOWER_SUCCESS: {
+      return {
+        ...state,
+        me: {
+          ...state.me,
+          Followers: state.me.Followers.filter(v => v.id !== action.data),
+        },
+        followerList: state.followerList.filter(v => v.id !== action.data)
+      };
+    }
+    case REMOVE_FOLLOWER_FAILURE: {
+      return {
+        ...state
+      }
+    }
+    case EDIT_NICKNAME_REQUEST: {
+      return {
+        ...state,
+        isEditingNickname: true,
+        editNicknameErrorReason: ''
+      }
+    }
+    case EDIT_NICKNAME_SUCCESS: {
+      return {
+        ...state,
+        isEditingNickname : false,
+        me: {
+          ...state.me,
+          nickname: action.data
+        }
+      }
+    }
+    case EDIT_NICKNAME_FAILURE: {
+      return {
+        ...state,
+        isEditingNickname: false,
+        editNicknameErrorReason: action.error
+      }
     }
     default: {
       return {
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -20,7 +20,25 @@ import {
   LOG_OUT_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAILURE,
-  LOAD_USER_REQUEST
+  LOAD_USER_REQUEST,
+  FOLLOW_USER_SUCCESS,
+  FOLLOW_USER_FAILURE,
+  FOLLOW_USER_REQUEST,
+  UNFOLLOW_USER_SUCCESS,
+  UNFOLLOW_USER_FAILURE,
+  UNFOLLOW_USER_REQUEST,
+  LOAD_FOLLOWERS_SUCCESS,
+  LOAD_FOLLOWERS_FAILURE,
+  LOAD_FOLLOWERS_REQUEST,
+  LOAD_FOLLOWINGS_SUCCESS,
+  LOAD_FOLLOWINGS_FAILURE,
+  LOAD_FOLLOWINGS_REQUEST,
+  REMOVE_FOLLOWER_SUCCESS,
+  REMOVE_FOLLOWER_FAILURE,
+  REMOVE_FOLLOWER_REQUEST,
+  EDIT_NICKNAME_SUCCESS,
+  EDIT_NICKNAME_FAILURE,
+  EDIT_NICKNAME_REQUEST
 } from "../reducers/user";
 
 import axios from "axios";
@@ -143,28 +161,186 @@ function* watchLoadUser() {
   yield takeEvery(LOAD_USER_REQUEST, loadUser);
 }
 
+function followAPI(userId) {
+  // 서버에 요청을 보내는 부분
+  return axios.post(`/user/${userId}/follow`,{}, {
+    withCredentials: true,
+  });
+}
 
-//function* watchHello() {
-//  yield takeLatest(HELLO_SAGA, hello);
-//}
+function* follow(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(followAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: FOLLOW_USER_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: FOLLOW_USER_FAILURE,
+      error: e,
+    });
+  }
+}
 
-//function* hello() {
-//  yield delay(1000);
-//  yield put({
-//    type: "BYE_SAGA"
-//  });
-//}
+function* watchFollow() {
+  yield takeEvery(FOLLOW_USER_REQUEST, follow);
+}
 
-//function* watchHello() {
-//  while (true) {
-//    yield take(HELLO_SAGA);
-//    console.log(1);
-//    console.log(2);
-//    console.log(3);
-//    console.log(4);
-//  }
-// }
+function unfollowAPI(userId) {
+  // 서버에 요청을 보내는 부분
+  return axios.delete(`/user/${userId}/follow`, {
+    withCredentials: true,
+  });
+}
+
+function* unfollow(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(unfollowAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: UNFOLLOW_USER_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: UNFOLLOW_USER_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchUnfollow() {
+  yield takeEvery(UNFOLLOW_USER_REQUEST, unfollow);
+}
+
+function loadFollowersAPI(userId) {
+  // 서버에 요청을 보내는 부분
+  return axios.get(`/user/${userId}/followers`, {
+    withCredentials: true,
+  });
+}
+
+function* loadFollowers(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(loadFollowersAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: LOAD_FOLLOWERS_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: LOAD_FOLLOWERS_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchLoadFollowers() {
+  yield takeEvery(LOAD_FOLLOWERS_REQUEST, loadFollowers);
+}
+
+
+function loadFollowingsAPI(userId) {
+  // 서버에 요청을 보내는 부분
+  return axios.get(`/user/${userId}/followings`, {
+    withCredentials: true,
+  });
+}
+
+function* loadFollowings(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(loadFollowingsAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: LOAD_FOLLOWINGS_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: LOAD_FOLLOWINGS_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchLoadFollowings() {
+  yield takeEvery(LOAD_FOLLOWINGS_REQUEST, loadFollowings);
+}
+
+function removeFollowerAPI(userId) {
+  // 서버에 요청을 보내는 부분
+  return axios.delete(`/user/${userId}/follower`, {
+    withCredentials: true,
+  });
+}
+
+function* removeFollower(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(removeFollowerAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: REMOVE_FOLLOWER_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: REMOVE_FOLLOWER_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchRemoveFollowers() {
+  yield takeEvery(REMOVE_FOLLOWER_REQUEST, removeFollower);
+}
+
+function editNicknameAPI(nickname) {
+  // 서버에 요청을 보내는 부분
+  return axios.patch('/user/nickname',{ nickname }, {
+    withCredentials: true,
+  });
+}
+
+function* editNickname(action) {
+  try {
+    // yield call(loadUserAPI);
+    const result = yield call(editNicknameAPI, action.data);
+    yield put({ // put은 dispatch 동일
+      type: EDIT_NICKNAME_SUCCESS,
+      data: result.data
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
+    yield put({
+      type: EDIT_NICKNAME_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchEditNickname() {
+  yield takeEvery(EDIT_NICKNAME_REQUEST, editNickname);
+}
 
 export default function* userSaga() {
-  yield all([fork(watchLogIn), fork(watchSignUp), fork(watchLogOut), fork(watchLoadUser)]);
+  yield all([
+    fork(watchLogIn), 
+    fork(watchSignUp), 
+    fork(watchLogOut), 
+    fork(watchLoadUser),
+    fork(watchFollow),
+    fork(watchUnfollow),
+    fork(watchLoadFollowers),
+    fork(watchLoadFollowings),
+    fork(watchRemoveFollowers),
+    fork(watchEditNickname)
+  ])
 }
