6c95131e1f1999454865dc2e6bb2216141d2cfef
KIMINJOONG <ijboym216@gmail.com>
2019-07-22 08:22:25 +0900

    control head tag with react-helmet

--- front/pages/post.js
+++ front/pages/post.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import { LOAD_POST_REQUEST } from '../reducers/post';
 const Post = ({ id }) => {
     const { singlePost } = useSelector(state => state.post);
     return (
       <>
+        {/* 검색엔진을 위해서 작성해줌 */}
+        <Helmet 
+            title={`${singlePost.User.nickname}님의 글`}
+            description={singlePost.content}
+            meta={[{
+                name: 'description', content: singlePost.content
+            }, {
+                property: 'og:title', content: `${singlePost.User.nickname}님의 글`,
+            }, {
+                property: 'og:description', content: singlePost.content
+            }, {
+                property: 'og:image', content: singlePost.Images[0] && `http://localhost:3065/${singlePost.Images[0].src}`
+            }, {
+                property: 'og:url', content: `http://localhost:3060/post/${id}`,
+            }]}
+        />
         <div>
             {singlePost.content}
         </div>
