fd087c140722d7cb73cf13fb473a0ee9ec852418
KIMINJOONG <ijboym216@gmail.com>
2019-08-16 07:28:54 +0900

    styled-components ssr

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Document, { Main, NextScript } from 'next/document';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
     static getInitialProps(context) {
-        const page = context.renderPage((App) => (props) => <App { ...props }/>);
-        return { ...page, helmet: Helmet.renderStatic() };
+        const sheet = new ServerStyleSheet;
+        const page = context.renderPage((App) => (props) => sheet.collectStyles(<App { ...props }/>));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, helmet: Helmet.renderStatic(), styleTags };
     }
     render(){
         const { htmlAttributes, bodyAttributes, ...helmet } = this.props.helmet;
@@ -15,6 +18,7 @@ class MyDocument extends Document {
         return (
             <html {...htmlAttrs }>
                 <head>
+                    {this.props.styleTags}
                     {Object.values(helmet).map(el => el.toComponent())}
                 </head>
                 <body {...bodyAttrs}>
