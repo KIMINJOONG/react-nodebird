cbd90847bba90e16309969ad8db4f774c13fd4df
KIMINJOONG <ijboym216@gmail.com>
2019-08-23 21:24:20 +0900

    next-bundle-analyzer

--- front/next.config.js
+++ front/next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+module.exports = withBundleAnalyzer({
     distDir: '.next',
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+        server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html'
+        },
+        browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+        }
+    },
     webpack(config) {
         const prod = process.env.NODE_ENV === 'production';
         console.log(config);
@@ -9,4 +23,4 @@ module.exports = {
             devtool: prod ? 'hidden-source-map' : 'eval', // hidden-source-map : 소스코드 숨기면서 에러시 소스맵제공 eval: 빠르게 웹팩 적용
         }
     }
-};
+});
